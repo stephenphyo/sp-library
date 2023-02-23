51032484e5990476b20cd4407976a456f391381c
STEPHEN PHYO <stephenphyo2019.official@gmail.com>
2023-02-23 03:03:33 +0000

    v02: 2022-02-23

--- sp-js-library/sp-js-utils/generateRandom.js
+++ sp-js-library/sp-js-utils/generateRandom.js
@@ -1,4 +1,4 @@
-const generateRandom = (min, max, decimal) => {
+const generateRandom = (min, max, decimal = 0) => {
     if (decimal < 0) return null;
     return (Math.floor((Math.random() * (max - min) + min) * (10 ** decimal)) / (10 ** decimal)).toFixed(decimal);
 };
